4c565e0b15839128f6fbfb94db09ff2b4c3acfd1
zzj <912290893@qq.com>
2023-11-12 22:05:34 +0800

    export package cmake_language

---
+++ Supplementary-practice/export/EXPORT/Add.cpp
@@ -0,0 +1,9 @@
+//
+// Created by zzj on 2023/11/12.
+//
+
+
+int add(int i, int j) {
+    return i + j;
+}
+
---
+++ Supplementary-practice/export/PACKAGE/main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by zzj on 2023/11/12.
+//
+#include <iostream>
+
+
+using namespace std;
+
+int add(int i, int j);
+
+int main() {
+
+    cout << add(1, 2) << endl;
+
+    return 0;
+}
+
+
